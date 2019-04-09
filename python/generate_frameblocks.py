9260c47849103cdacc1956015e323e815dc2e39f
pixarninja <wesharris505@gmail.com>
2019-04-09 10:00:54 -0600

    Prepare for mid-term presentation

--- python/generate_frameblocks.py
+++ python/generate_frameblocks.py
@@ -1,7 +1,6 @@
 import os as os
 import cv2 as cv2
 import numpy as np
-from matplotlib import pyplot as plt
 
 block_dim = 32
 block_offset = 1
@@ -11,14 +10,8 @@ img_str_1 = './images/samples/frame_1_complex.jpg'
 img_str_2 = './images/samples/frame_3_complex.jpg'
 img_str_out = './images/output_complex.jpg'
 img_str_roi = './images/frameblock_roi_complex.jpg'
-os.system('rm -rf %s' % './images/blocks/buffer/frames/')
-os.mkdir('./images/blocks/buffer/frames/')
 
-# Delete previously calculated frames, buffers, and shadows.
-os.system('rm -rf %s' % './images/blocks/complex/')
-os.mkdir('./images/blocks/complex/')
-os.system('rm -rf %s' % './images/blocks/buffer/frames/')
-os.mkdir('./images/blocks/buffer/frames/')
+# Delete previously found shadows, frames and buffers are deleted later.
 os.system('rm -rf %s' % './images/blocks/buffer/shadows/')
 os.mkdir('./images/blocks/buffer/shadows/')
 
@@ -27,19 +20,13 @@ height_1, width_1 = img_1.shape[:2]
 img_2 = cv2.cvtColor(cv2.imread(img_str_2), cv2.COLOR_BGR2RGB)
 height_2, width_2 = img_2.shape[:2]
 
-# Choose smallest boundaries
+# Choose smallest boundaries.
 height = height_1
 width = width_1
 if height_1 > height_2:
     height = height_2
 if width_1 > width_2:
     width = width_2
-
-# Plot images for reference.
-plt.subplot(121),plt.imshow(img_1),plt.title('Input Image 1')
-plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(img_2),plt.title('Input Image 2')
-plt.xticks([]), plt.yticks([])
 
 img_out = np.ones((height, width, 3), np.uint8)
 total_pixel_sum = 0
@@ -59,10 +46,8 @@ for y in range(0, height):
         bw_val = 255 - bw_val
         img_out[y, x] = bw_val
 
-# Write image and plot to screen.
+# Write image.
 cv2.imwrite(img_str_out, img_out)
-plt.subplot(111),plt.imshow(img_out),plt.title('Calculated Image for Frameblock Generation')
-plt.xticks([]), plt.yticks([])
 
 # Calculate the pixel_ratio.
 print("Total pixel sum: " + str(total_pixel_sum))
@@ -82,11 +67,11 @@ pixel_sum = 0
 cap = np.power(block_dim, 2) * 255 * pixel_ratio
 print("Cap found: " + str(cap))
 
-# Delete previously found buffers but leave images
-os.system('rm -rf %s' % './images/blocks/buffer/shadows/')
-os.mkdir('./images/blocks/buffer/shadows/')
+# Delete previously found frames and buffers.
 os.system('rm -rf %s' % './images/blocks/complex/')
 os.mkdir('./images/blocks/complex/')
+os.system('rm -rf %s' % './images/blocks/buffer/frames/')
+os.mkdir('./images/blocks/buffer/frames/')
 
 # Find the Region Of Interest (ROI).
 while bottom < height:
@@ -140,43 +125,14 @@ while bottom < height:
                 cv2.imwrite(img_buff_str, img_roi_buff)
         frame_index += 1
         
-        # Shift horizontally
+        # Shift horizontally.
         left += int(block_dim / block_offset)
         right += int(block_dim / block_offset)
-    # Shift vertically
+    # Shift vertically.
     top += int(block_dim / block_offset)
     bottom += int(block_dim / block_offset)
     left = 0
     right = block_dim
 
+# Write image.
 cv2.imwrite(img_str_roi, img_roi_all)
-plt.subplot(111),plt.imshow(img_roi_all),plt.title('Frameblocks Selected (' + str(frame_index - 1) + ')')
-plt.xticks([]), plt.yticks([])
-
-frame_index = 1
-img_block_str = './images/blocks/complex/block' + str(frame_index) + '_1.jpg'
-while not os.path.exists(img_block_str):
-    frame_index += 1
-    img_block_str = './images/blocks/complex/block' + str(frame_index) + '_1.jpg'
-img_block_1 = cv2.cvtColor(cv2.imread(img_block_str), cv2.COLOR_BGR2RGB)
-img_block_str = './images/blocks/complex/block' + str(frame_index) + '_2.jpg'
-img_block_2 = cv2.cvtColor(cv2.imread(img_block_str), cv2.COLOR_BGR2RGB)
-plt.subplot(121),plt.imshow(img_block_1),plt.title('Frameblock 1 From Image 1')
-plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(img_block_2),plt.title('Frameblock 1 From Image 2')
-plt.xticks([]), plt.yticks([])
-plt.show()
-
-frame_index += 1
-img_block_str = './images/blocks/complex/block' + str(frame_index) + '_1.jpg'
-while not os.path.exists(img_block_str):
-    frame_index += 1
-    img_block_str = './images/blocks/complex/block' + str(frame_index) + '_1.jpg'
-img_block_1 = cv2.cvtColor(cv2.imread(img_block_str), cv2.COLOR_BGR2RGB)
-img_block_str = './images/blocks/complex/block' + str(frame_index) + '_2.jpg'
-img_block_2 = cv2.cvtColor(cv2.imread(img_block_str), cv2.COLOR_BGR2RGB)
-plt.subplot(121),plt.imshow(img_block_1),plt.title('Frameblock 2 From Image 1')
-plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(img_block_2),plt.title('Frameblock 2 From Image 2')
-plt.xticks([]), plt.yticks([])
-plt.show()
