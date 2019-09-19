e5c0f4e8a94b9a07baefcdbff0b1739a420fed2b
pixarninja <wesharris505@gmail.com>
2019-09-19 15:05:17 -0700

    Work on dataset generation

--- python/generate_frameblocks.py
+++ python/generate_frameblocks.py
@@ -1,29 +1,65 @@
 import os as os
+import glob as glob
 import cv2 as cv2
 import numpy as np
 
+# Initialize seed variables.
 block_dim = 32
 block_offset = 1
 
+# Initialize path prefixes.
+path_prefix = '../jupyter/images/'
+buffer_prefix = path_prefix + 'buffer/'
+
+# Initialize path variables.
+frames_path = path_prefix + 'glass/'
+training_path = path_prefix + 'blocks/'
+shadow_img_path = path_prefix + 'shadow/'
+roi_img_path = path_prefix + 'roi/'
+shadow_buff_path = buffer_prefix + 'shadows/'
+frame_buff_path = buffer_prefix + 'frames/'
+
 # Delete previously output frameblocks, and buffer shadows and buffer frames.
-os.system('rm -rf %s' % './images/blocks/pairs/*')
-os.system('rm -rf %s' % './images/blocks/buffer/shadows/*')
-os.system('rm -rf %s' % './images/blocks/buffer/frames/*')
-os.system('rm -rf %s' % './images/shadow/*')
-os.system('rm -rf %s' % './images/roi/*')
+if os.path.exists(training_path):
+    filelist = glob.glob(training_path + '*')
+    for file in filelist:
+        os.remove(file)
+else:
+    os.mkdir(training_path)
+
+if not os.path.exists(buffer_prefix):
+    os.mkdir(buffer_prefix)
+    os.mkdir(shadow_buff_path)
+    os.mkdir(frame_buff_path)
+else:
+    if os.path.exists(shadow_buff_path):
+        filelist = glob.glob(shadow_buff_path + '*')
+        for file in filelist:
+            os.remove(file)
+    else:
+        os.mkdir(shadow_buff_path)
+
+    if os.path.exists(frame_buff_path):
+        filelist = glob.glob(frame_buff_path + '*')
+        for file in filelist:
+            os.remove(file)
+    else:
+        os.mkdir(frame_buff_path)
 
 # Setup main loop to process all frames in an animation.
-frames = os.listdir('./images/frames/')
+frames = os.listdir(frames_path)
 frames.sort()
+block_index = 1
 
 # Process each frame.
 for frame_index in range(0, len(frames), 2):
     frame = frames[frame_index]
+    img_str_out = training_path + 'block' + str(block_index) + 'frame' + str(frame_index + 1)
 
     # If the frame index is 0, store all frameblocks.
     if frame_index < 1:
         # Initialize seed variables.
-        img_str_1 = './images/frames/' + frames[frame_index]
+        img_str_1 = frames_path + frames[frame_index]
 
         # Choose smallest boundaries.
         img_1 = cv2.cvtColor(cv2.imread(img_str_1), cv2.COLOR_BGR2RGB)
@@ -47,16 +83,10 @@ for frame_index in range(0, len(frames), 2):
 
                 # ROI pixel processing
                 print(str(block_index) + ". Frameblock: (" + str(left) + ", " + str(top) + "), (" + str(right) + ", " + str(bottom) + "))")
-                
-                # Setup storage.
-                img_out_str = './images/blocks/pairs/block' + str(block_index) + '/frame' + str(frame_index + 1)
-                if not os.path.exists(img_out_str):
-                    os.mkdir('./images/blocks/pairs/block' + str(block_index))
-                    os.mkdir(img_out_str)
 
                 # Store window contents as image.
                 img_roi = img_1[top:bottom, left:right]
-                cv2.imwrite(img_out_str + '/end.jpg', img_roi)
+                cv2.imwrite(img_str_out + '_end.jpg', img_roi)
 
                 # Increase frameblock index.
                 block_index += 1
@@ -74,10 +104,10 @@ for frame_index in range(0, len(frames), 2):
     # Otherwise process as normal.
     else:
         # Initialize seed variables.
-        img_str_1 = './images/frames/' + frames[frame_index - 2]
-        img_str_2 = './images/frames/' + frames[frame_index]
-        img_str_shd = './images/shadow/frame' + str(frame_index) + '.jpg'
-        img_str_roi = './images/roi/frame' + str(frame_index) + '.jpg'
+        img_str_1 = frames_path + frames[frame_index - 2]
+        img_str_2 = frames_path + frames[frame_index]
+        img_str_shd = shadow_img_path + 'frame' + str(frame_index) + '.jpg'
+        img_str_roi = roi_img_path + 'frame' + str(frame_index) + '.jpg'
 
         img_1 = cv2.cvtColor(cv2.imread(img_str_1), cv2.COLOR_BGR2RGB)
         img_1 = cv2.resize(img_1, (0,0), fx=0.5, fy=0.5) 
@@ -140,7 +170,7 @@ for frame_index in range(0, len(frames), 2):
                 found_x = False
                 dirty = False
                 pixel_sum = 0
-                img_buff_str = './images/blocks/buffer/shadows/block' + str(block_index) + '.jpg'
+                img_buff_str = shadow_buff_path + 'block' + str(block_index) + '.jpg'
                 img_buff = cv2.imread(img_buff_str)
                 if img_buff is None:
                     img_buff = np.zeros((block_dim, block_dim, 3), np.uint8)
@@ -166,14 +196,9 @@ for frame_index in range(0, len(frames), 2):
                             cv2.putText(img_roi_all, str(block_index), (left + 3, bottom - 3), cv2.FONT_HERSHEY_PLAIN, 0.75, (255, 0, 0), 1, 1)
                             print(str(block_index) + ". Sum: " + str(pixel_sum) + ", Frameblock: (" + str(left) + ", " + str(top) + "), (" + str(right) + ", " + str(bottom) + "))")
                             
-                            # Setup storage.
-                            img_out_str = './images/blocks/pairs/block' + str(block_index) + '/frame' + str(frame_index + 1)
-                            if not os.path.exists(img_out_str):
-                                os.mkdir(img_out_str)
-
                             # Store window contents as image.
                             img_roi = img_2[top:bottom, left:right]
-                            cv2.imwrite(img_out_str + '/end.jpg', img_roi)
+                            cv2.imwrite(img_str_out + '_end.jpg', img_roi)
                             
                             # Exit both for loops.
                             found_x = True
@@ -188,31 +213,22 @@ for frame_index in range(0, len(frames), 2):
                         os.remove(img_buff_str)
 
                     # If there is a buffered frame for the block use it as the starting frame.
-                    img_buff_str = './images/blocks/buffer/frames/block' + str(block_index) + '.jpg'
+                    img_buff_str = frame_buff_path + 'block' + str(block_index) + '.jpg'
                     if os.path.exists(img_buff_str):
-                        # Setup storage.
-                        img_out_str = './images/blocks/pairs/block' + str(block_index) + '/frame' + str(frame_index + 1)
-                        if not os.path.exists(img_out_str):
-                            os.mkdir(img_out_str)
-
                         # Move and rename file.
-                        os.rename(img_buff_str, img_out_str + '/start.jpg')
+                        os.rename(img_buff_str, img_str_out + '_start.jpg')
 
                     # Otherwise export the ROI of the first image as the starting frame.
                     else:
-                        # Setup storage.
-                        img_out_str = './images/blocks/pairs/block' + str(block_index) + '/frame' + str(frame_index + 1)
-                        if not os.path.exists(img_out_str):
-                            os.mkdir(img_out_str)
-
                         # Store window contents as image.
                         img_roi = img_1[top:bottom, left:right]
-                        cv2.imwrite(img_out_str + '/start.jpg', img_roi)
+                        cv2.imwrite(img_str_out + '_start.jpg', img_roi)
 
                 # Otherwise export the shadow and frame ROI to be used next time.
                 else:
                     # If the shadow image already exists, update it using a linear add.
-                    img_buff_str = './images/blocks/buffer/shadows/block' + str(block_index) + '.jpg'
+                    img_buff_str = shadow_buff_path + 'block' + str(block_index) + '.jpg'
+
                     if os.path.exists(img_buff_str):
                         # Add the values of the current shadow image and previous shadow image.
                         img_prev_buff = cv2.imread(img_buff_str)
@@ -222,9 +238,9 @@ for frame_index in range(0, len(frames), 2):
                         cv2.imwrite(img_buff_str, img_buff)
                         print('Updated shadow image, \'block' + str(block_index) + '.jpg\'')
 
-                        # Draw Shadow ROI on clone image.
-                        cv2.rectangle(img_roi_all, (left + 1, top + 1), (right - 1, bottom - 1), (255, 0, 255), 1)
-                        cv2.putText(img_roi_all, str(block_index), (left + 3, bottom - 3), cv2.FONT_HERSHEY_PLAIN, 0.75, (255, 0, 255), 1, 1)
+                        #    # Draw Shadow ROI on clone image.
+                        #    cv2.rectangle(img_roi_all, (left + 1, top + 1), (right - 1, bottom - 1), (255, 0, 255), 1)
+                        #    cv2.putText(img_roi_all, str(block_index), (left + 3, bottom - 3), cv2.FONT_HERSHEY_PLAIN, 0.75, (255, 0, 255), 1, 1)
 
                     # Else if a black pixel was found write a new shadow image.
                     elif dirty:
@@ -232,20 +248,20 @@ for frame_index in range(0, len(frames), 2):
                         print('Wrote new shadow image, \'block' + str(block_index) + '.jpg\'')
 
                         # Store frame ROI image.
-                        img_buff_str = './images/blocks/buffer/frames/block' + str(block_index) + '.jpg'
+                        img_buff_str = frame_buff_path + 'block' + str(block_index) + '.jpg'
                         img_roi = img_1[top:bottom, left:right]
                         cv2.imwrite(img_buff_str, img_roi)
 
-                        # Draw Shadow ROI on clone image.
-                        cv2.rectangle(img_roi_all, (left + 1, top + 1), (right - 1, bottom - 1), (0, 0, 0), 1)
-                        cv2.putText(img_roi_all, str(block_index), (left + 3, bottom - 3), cv2.FONT_HERSHEY_PLAIN, 0.75, (0, 0, 0), 1, 1)
+                        #    Draw Shadow ROI on clone image.
+                        #    cv2.rectangle(img_roi_all, (left + 1, top + 1), (right - 1, bottom - 1), (0, 0, 0), 1)
+                        #    cv2.putText(img_roi_all, str(block_index), (left + 3, bottom - 3), cv2.FONT_HERSHEY_PLAIN, 0.75, (0, 0, 0), 1, 1)
 
                     # Otherwise notify that the block has been processed with no export.
                     # Commented out for now to save time printing.
                     #else:
                     #    print(str(block_index) + ". No export")
 
-                    #    Draw Shadow ROI on clone image.
+                    #    # Draw Shadow ROI on clone image.
                     #    cv2.rectangle(img_roi_all, (left + 1, top + 1), (right - 1, bottom - 1), (0, 0, 255), 1)
                     #    cv2.putText(img_roi_all, str(block_index), (left + 3, bottom - 3), cv2.FONT_HERSHEY_PLAIN, 0.75, (0, 0, 255), 1, 1)
 
